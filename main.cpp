35980966acba42da5d3df31e7b34c6196fc07cfb
Daniel Fernando Sosa Suárez <danielsosa117@javerianacali.edu.co>
2018-10-30 15:50:55 -0500

    mainasd

---
+++ main.cpp
@@ -0,0 +1,24 @@
+holiiii
+
+dfadf
+adfa
+#ifndef adfads
+fa
+#ifndef ad
+fa
+dfadfdfa
+#define ad
+fa
+dfadfdfa value
+#endif
+#define adfads
+fa
+#ifndef ad
+fa
+dfadfdfa
+#define ad
+fa
+dfadfdfa value
+#endif value
+#endif
+sadasdasdasdadasdasdasdasdsadas
