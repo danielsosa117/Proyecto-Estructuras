01ecb40d5708140860a3da815291147a09073c38
juanreyesley <38504405+juanreyesley@users.noreply.github.com>
2018-11-01 14:21:16 -0500

    expresion

---
+++ Expresion.h
@@ -0,0 +1,18 @@
+#ifndef EXPRESION_H
+#define EXPRESION_H
+
+#include <iostream>
+#include <string>
+
+class Expresion {
+
+	private:
+
+		string ecuacion;
+		Expresion expresion;
+
+	public:
+
+};
+
+#endif
